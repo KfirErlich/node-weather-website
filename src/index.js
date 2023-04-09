4984588a51a84114ec1c4717e977983c0ac58323
Kfir <kfir.erlich28@gmail.com>
2023-04-09 15:30:52 +0300

    changed to index file

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const request = require("postman-request");
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
-const app = express()
+const index = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -16,36 +16,36 @@ const viewPath = path.join(__dirname,'../tamplates/views')
 const partialsPath = path.join(__dirname, '../tamplates/partials')
 
 // Setup handlers engine and views location
-app.set('view engine','hbs')
-app.set('views',viewPath)
+index.set('view engine','hbs')
+index.set('views',viewPath)
 hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+index.use(express.static(publicDirectoryPath))
 
 
-app.get('',(req,res) => {
+index.get('',(req, res) => {
     res.render('index', {
         title: 'Weather',
         name: 'Kfir Erlich'
     })
 })
 
-app.get('/about', (req,res) => {
+index.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
         name: 'Kfir Erlich'
     })
 })
 
-app.get('/help', (req, res) => {
+index.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
         name: 'Kfir Erlich'
     })
 })
 
-app.get('/weather', (req,res) => {
+index.get('/weather', (req, res) => {
     if(!req.query.address){
         return res.send({
             error: 'An address must be provided'
@@ -72,7 +72,7 @@ app.get('/weather', (req,res) => {
 
 })
 
-app.get('/product', (req, res) => {
+index.get('/product', (req, res) => {
     if(!req.query.search){
        return res.send({
             error: 'you must provide a search term'
@@ -84,7 +84,7 @@ app.get('/product', (req, res) => {
     })
 })
 
-app.get('/help/*',(req,res) => {
+index.get('/help/*',(req, res) => {
     res.render('404',{
         title: '404',
         name: 'Kfir Erlich',
@@ -92,7 +92,7 @@ app.get('/help/*',(req,res) => {
     })
 })
 
-app.get('*', (req,res) => {
+index.get('*', (req, res) => {
     res.render('404',{
         title: '404',
         name: 'Kfir Erlich',
@@ -100,6 +100,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
+index.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
